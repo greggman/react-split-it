68b130c81193005251ee4bc0ba1e82f945b16d04
Gregg Tavares <github@greggman.com>
2020-10-28 21:40:54 +0900

    formatting

--- src/split.js
+++ src/split.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+
 import {classNames} from './css-utils.js';
 import Gutter from './gutter.js';
 import moveGuttersComputeNewSizes from './move-gutters-compute-new-sizes.js';
